bfeaacaa5f86d34bcec17aa9b41d4c9780de7942
Disni Jayawickrama <86881451+Disnidj@users.noreply.github.com>
2022-08-27 15:35:28 +0530

    commit models of attandance and leave req

---
+++ Backend/models/EmpAttendace.js
@@ -0,0 +1,50 @@
+//import mongoose
+
+const mongoose=require("mongoose");
+
+//make a db schema to store employee attendace data
+
+const EmpAttendaceSchema = new mongoose.Schema({
+    Emp_Name:{
+        type:String,
+        required:true
+    },
+
+    Emp_ID:{
+        type:String,
+        required:true
+    },
+
+    Date:{
+        type:String,
+        required:true
+    },
+
+    Shift:{
+        type:String,
+        required:true
+    },
+
+    Time_In:{
+        type:String,
+        required:true
+    },
+
+    Time_Out:{
+        type:String,
+        required:true
+    },
+
+    Total_Hours_per_Day:{
+        type:Number,
+        required:true
+    },
+
+    Total_Hours_per_Month:{
+        type:Number,
+        
+    }
+});
+
+//pass the schema
+module.exports=mongoose.model("EmpAttendace",EmpAttendaceSchema);
---
+++ Backend/models/EmpLeaveForm.js
@@ -0,0 +1,44 @@
+//import mongoose
+const mongoose = require("mongoose");
+
+//create db schema to store data
+const EmpLeaveDataSchema = new mongoose.Schema({
+    Today:{
+        type:String,
+        require:true
+    },
+     
+    Emp_Name:{
+        type:String,
+        require:true
+    },
+
+    Emp_ID:{
+        type:String,
+        require:true
+    },
+
+    Leave_Reason:{
+        type:String,
+        require:true
+    },
+
+    Leave_From:{
+        type:String,
+        require:true
+    },
+
+    Leave_To:{
+        type:String,
+        require:true
+    },
+
+    Approval:{
+        type:String
+    },
+    Comments:{
+        type:String
+    }
+})
+
+module.exports=mongoose.model("Leave_From",EmpLeaveDataSchema);
