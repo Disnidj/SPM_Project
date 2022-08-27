9d4ea9e0ec8305ea05b4bc6b6bc7df0483b2165a
Disni Jayawickrama <86881451+Disnidj@users.noreply.github.com>
2022-08-27 15:36:26 +0530

    commit routes of attendace and leave req

---
+++ Backend/routes/EmpAttendace.js
@@ -0,0 +1,100 @@
+//import express
+const express = require("express");
+
+//import db schema
+const EmpAttendance =require("../models/EmpAttendace");
+
+//give access to request function through express router
+const router=express.Router();
+
+//Create function
+router.post("/EmpAttendace/Save",(req,res)=>{
+    let newAttendace = new EmpAttendance(req.body);
+
+    newAttendace.save((err)=>{
+
+        if(err){
+            return res.status(400).json(
+                {error:err}
+            )
+        }
+        return res.status(200).json({
+            success:"Attendace marked successfully"
+        });
+    });
+});
+
+//Get all Attendance results
+
+router.get("/GetAllAttendance",(req,res)=>{
+    EmpAttendance.find().exec((err,allAttendace)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+            return res.status(200.).json({
+                success:true,
+                exixtingData:allAttendace
+            });
+        
+    });
+});
+
+//get one attendance
+
+router.get("/GetOneAttendance/:id",(req,res)=>{
+    let Attendace_ID= req.params.id;
+
+    EmpAttendance.findById(Attendace_ID,(err,oneAttendance)=>{
+        if(err){
+            return res.status(400).json({
+                success:false,
+                err
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            oneAttendance
+        });
+    });
+});
+
+
+//update an attendance
+
+router.put("/UpdateAttendance/:id",(req,res)=>{
+    EmpAttendance.findByIdAndUpdate(
+        req.params.id, {$set:req.body},
+    (err,updateAttendance)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+            
+        }
+        return res.status(200).json({
+            success:"Updated Successfully"
+        });
+    });
+
+});
+
+
+//delete an attendance
+
+router.delete("/DeleteAttendance/:id", (req,res)=>{
+    EmpAttendance.findByIdAndRemove(req.params.id).exec((err,deletedAttendance)=>{
+        if(err){
+            return res.status(400).json({
+                message:"Delete was unsuccessful",err
+            });
+            
+        }
+        return res.json({
+            message:"Deleted Successfully",deletedAttendance
+        });
+    });
+});
+
+module.exports=router;
---
+++ Backend/routes/EmpLeaveForm.js
@@ -0,0 +1,107 @@
+//import express
+const express =require("express")
+
+//import models
+const EmpLeaveRequest = require("../models/EmpLeaveForm");
+
+//Declare express routers to connect with functions
+const router=express.Router();
+
+//Create Leave request
+
+router.post('/EmpLeaveReq/Save',(req,res)=>{
+    let newLeaveForm= new EmpLeaveRequest(req.body);
+
+    newLeaveForm.save((err)=>{
+
+        if(err){
+        return res.status(400).json({
+            error:err
+        });
+        }
+        return res.status(200).json({
+            success:"Form Submitted Successfully"
+        });
+    });
+});
+
+
+//get all leave forms
+
+router.get("/GetAllLeaveReq",(req,res)=>{
+    EmpLeaveRequest.find().exec((err,GetAllReq)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            GetAllReq
+        });
+    });
+});
+
+//get one leave request
+
+router.get("/GetOneLeaveRequest/:id",(req,res)=>{
+    let ReqID= req.params.id;
+
+    EmpLeaveRequest.findById(ReqID,(err,OneLeaveReq)=>{
+        if(err){
+            return res.status(400).json({
+                success:false,
+                err
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            OneLeaveReq
+        });
+    });
+
+});
+
+//update a leave form
+
+router.put("/UpdateLeaveReq/:id",(req,res)=>{
+    EmpLeaveRequest.findByIdAndUpdate(
+        req.params.id, {$set:req.body},
+    (err,updateReq)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+            
+        }
+        return res.status(200).json({
+            success:"Updated Successfully"
+        });
+    });
+
+});
+
+//delete a request
+
+router.delete("/DeleteLeaveReq/:id", (req,res)=>{
+    EmpLeaveRequest.findByIdAndRemove(req.params.id).exec((err,deletedReq)=>{
+        if(err){
+            return res.status(400).json({
+                message:"Delete was unsuccessful",err
+            });
+            
+        }
+        return res.json({
+            message:"Deleted Successfully",deletedReq
+        });
+    });
+});
+
+module.exports=router;
+
+
+
+
+
